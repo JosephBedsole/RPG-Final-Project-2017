9e1340a6cab1b8ad60101769676359519853db78
Joseph Bedsole <jbedsole@dmi-intertech.org>
2017-10-26 23:18:42 -0500

    Made scriptable objects for items and character inventories

---
+++ Assets/Joseph/ScriptableObjects/CharacterInventory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "inventory/CharacterInventory", order = 2)]
+public class CharacterInventory : ScriptableObject {
+
+	public Items helmet;
+	public Items sword;
+	public Items rightHand;
+	public Items rightAccessory;
+	public Items leftHand;
+	public Items leftAccessory;
+	public Items breastPlate;
+	public Items neckAccessory;
+	public Items legs;
+	public Items feet;
+
+}
---
+++ Assets/Joseph/ScriptableObjects/Items/Items.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[CreateAssetMenu(fileName = "Data", menuName = "inventory/Items", order = 1)]
+public class Items : ScriptableObject {
+
+	public string itemName;
+
+	public string itemType;
+
+	public Sprite combatSprite;   // For the fighting scenes;
+
+	public Sprite uiSprite;       // For the Inventory screen;
+
+	[Header("Nerd Stuff")]
+	public Sprite damageTypeSprite;
+	public string damageType;
+	public int damage;
+	public int speed;
+	public int critChance;
+	public int health;
+	public int armor;
+	public int magicResist;
+
+}
