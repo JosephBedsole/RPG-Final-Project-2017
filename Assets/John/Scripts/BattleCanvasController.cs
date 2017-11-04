83c31ba78e2175499ba942153b9e540c82af97af
John Hawley <jhawley318@gmail.com>
2017-11-03 23:44:31 -0500

    continuing work on canvascontroller, implemeting CT charge times

--- Assets/John/Scripts/BattleCanvasController.cs
+++ Assets/John/Scripts/BattleCanvasController.cs
@@ -5,7 +5,22 @@ using UnityEngine.UI;
 
 public class BattleCanvasController : MonoBehaviour {
 
+	public static BattleCanvasController instance = null;
+
 	public GameObject pointer;
+	public Image ct1;
+	public Image ct2;
+	public Image ct3;
+	public Image ct4;
+	[HideInInspector]
+	public float ct1Speed;
+	[HideInInspector]
+	public float ct2Speed;
+	[HideInInspector]
+	public float ct3Speed;
+	[HideInInspector]
+	public float ct4Speed;
+
 
 	[HideInInspector]
 	public enum Selection {none, pc, action, enemy}
@@ -15,11 +30,27 @@ public class BattleCanvasController : MonoBehaviour {
 	GameObject pcPanels;
 	GameObject enemyPanels;
 	GameObject enemySprites;
+
+	List<Image> ctList = new List<Image>(); // TODO: get cts onEnable instead of dragndrop
+	List<float> ctListSpeed = new List<float>();
 	
 
 	void OnEnable(){
+		if (instance == null) instance = this; 
+		else Destroy(gameObject); 
+
 		actionPanel = GameObject.Find("ActionPanel");
 		actionPanel.SetActive(false);
+		ctList.Add(ct1);
+		ctList.Add(ct2);
+		ctList.Add(ct3);
+		ctList.Add(ct4);
+		
+		for (int i = 0; i < ctList.Count; i++){
+			ctList[i].fillAmount = 0.3f;
+		}
+
+		SetCTImageFill(GetCTImage(ct1));
 	}
 
 	public void ButtonClicked(string s){
@@ -56,5 +87,12 @@ public class BattleCanvasController : MonoBehaviour {
 		}
 	}
 
+	public Image GetCTImage(Image ct){
+		return ctList[ctList.IndexOf(ct)];
+	}
+
+	public void SetCTImageFill(Image ct){
+		ctList[ctList.IndexOf(ct)].fillAmount = 1;
+	}
 
 }
