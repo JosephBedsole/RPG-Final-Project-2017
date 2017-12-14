7f63eb1dfd29346c8da86e1a327f9aa7c1ee0f05
Joseph Bedsole <jbedsole@dmi-intertech.org>
2017-12-13 23:16:06 -0600

    Created options menu and improved the look of the menus.

--- Assets/Joseph/Tiled Maps/NewCastleSheet.tsx
+++ Assets/Joseph/Tiled Maps/NewCastleSheet.tsx
@@ -3,7 +3,7 @@
  <image source="../Sprites/Castle2.png" width="512" height="512"/>
  <tile id="6">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="32" height="32"/>
+   <object id="2" x="6" y="0" width="26" height="32"/>
   </objectgroup>
  </tile>
  <tile id="7">
@@ -18,22 +18,22 @@
  </tile>
  <tile id="9">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="32" height="32"/>
+   <object id="2" x="0" y="0" width="26" height="32"/>
   </objectgroup>
  </tile>
  <tile id="22">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="32" height="32"/>
+   <object id="2" x="7" y="0" width="25" height="32"/>
   </objectgroup>
  </tile>
  <tile id="25">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="32" height="32"/>
+   <object id="2" x="0" y="0" width="25" height="32"/>
   </objectgroup>
  </tile>
  <tile id="38">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="32" height="32"/>
+   <object id="2" x="6" y="0" width="26" height="32"/>
   </objectgroup>
  </tile>
  <tile id="39">
@@ -50,7 +50,7 @@
   <objectgroup draworder="index">
    <object id="1" x="0" y="0" width="32" height="32"/>
    <object id="3" x="0" y="0" width="32" height="32"/>
-   <object id="4" x="0" y="0" width="32" height="32"/>
+   <object id="5" x="0" y="0" width="26" height="32"/>
   </objectgroup>
  </tile>
  <tile id="103">
@@ -58,19 +58,49 @@
    <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
+ <tile id="131">
+  <objectgroup draworder="index">
+   <object id="2" x="0" y="0" width="32" height="6"/>
+  </objectgroup>
+ </tile>
+ <tile id="132">
+  <objectgroup draworder="index">
+   <object id="2" x="1" y="0" width="28" height="5"/>
+  </objectgroup>
+ </tile>
  <tile id="133">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="7" width="32" height="3"/>
+   <object id="3" x="0" y="4" width="32" height="4"/>
   </objectgroup>
  </tile>
  <tile id="144">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="32" height="32"/>
+   <object id="3" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
  <tile id="145">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="32" height="32"/>
+   <object id="4" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="160">
+  <objectgroup draworder="index">
+   <object id="1" x="4" y="0" width="28" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="161">
+  <objectgroup draworder="index">
+   <object id="3" x="0" y="0" width="30" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="201">
+  <objectgroup draworder="index">
+   <object id="3" x="5" y="29" width="27" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="202">
+  <objectgroup draworder="index">
+   <object id="3" x="0" y="28" width="29" height="4"/>
   </objectgroup>
  </tile>
  <tile id="203">
@@ -78,16 +108,28 @@
    <object id="2" x="0" y="0" width="32" height="9"/>
   </objectgroup>
  </tile>
+ <tile id="217">
+  <objectgroup draworder="index">
+   <object id="3" x="2" y="0" width="30" height="5"/>
+  </objectgroup>
+ </tile>
+ <tile id="218">
+  <objectgroup draworder="index">
+   <object id="2" x="0" y="0" width="29" height="4"/>
+  </objectgroup>
+ </tile>
+ <tile id="236">
+  <objectgroup draworder="index"/>
+ </tile>
  <tile id="249">
   <objectgroup draworder="index">
    <object id="1" x="0" y="0" width="32" height="31"/>
-   <object id="2" x="2" y="1" width="28" height="30"/>
+   <object id="3" x="0" y="0" width="32" height="11"/>
   </objectgroup>
  </tile>
  <tile id="252">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="32" height="28"/>
-   <object id="2" x="2" y="0" width="28" height="28"/>
+   <object id="3" x="3" y="0" width="26" height="4"/>
   </objectgroup>
  </tile>
 </tileset>
