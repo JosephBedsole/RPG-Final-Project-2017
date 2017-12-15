109be9b270b9e3ae38359669fd27bcc561ea5627
Joseph Bedsole <jbedsole@dmi-intertech.org>
2017-12-14 20:11:44 -0600

    Alterned textures to look better.

--- Assets/Joseph/Tiled Maps/LargeTileSheet.tsx
+++ Assets/Joseph/Tiled Maps/LargeTileSheet.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="LargeTileSet" tilewidth="32" tileheight="32" tilecount="900" columns="30">
  <image source="../Sprites/hyptosis_tile-art-batch-1 (1).png" width="960" height="960"/>
+ <tile id="782">
+  <objectgroup draworder="index">
+   <object id="3" x="0" y="16" width="32" height="16"/>
+  </objectgroup>
+ </tile>
 </tileset>
